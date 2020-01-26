716c1500f95b39065433161411bf7ebe15fea50e
Edrees Jalili <edreesjalili@gmail.com>
2020-01-26 12:22:55 -0600

    Fix prettier, eslint integration

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const api = require('./api');
-const articles = require('./articles');
+const router = require("express").Router();
+const api = require("./api");
+const articles = require("./articles");
 
-router.use('/api', api);
-router.use('/articles', articles);
+router.use("/api", api);
+router.use("/articles", articles);
 
-router.use('/', (req, res) => res.redirect('/articles'));
-router.use('*', (req, res) => res.render('error'));
+router.use("/", (req, res) => res.redirect("/articles"));
+router.use("*", (req, res) => res.render("error"));
 
 module.exports = router;
