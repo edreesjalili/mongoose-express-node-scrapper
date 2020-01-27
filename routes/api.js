b6778d8019d458a5f3a1add32eef3d922e687721
Edrees Jalili <edreesjalili@gmail.com>
2020-01-26 19:19:56 -0600

    Add scrape functionality

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,6 @@
-const router = require('express').Router();
+const router = require('express').Router()
+const { handleScrape } = require('../controllers/scrape')
 
-module.exports = router;
+router.get('/scrape', handleScrape)
+
+module.exports = router
